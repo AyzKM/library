703565b120e7525638d2d7c163c154292acba631
Ayzada Karataeva <ayzadakarataeva@Ayzadas-MBP.netis>
2021-02-04 18:22:32 +0100

    created book addition

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from openpyxl import load_workbook
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return 'Test 1!!!'
+    return render_template("authors.html")
 
 @app.route("/books/")
 def books():
@@ -32,5 +32,17 @@ def authors():
     excel = load_workbook("tales.xlsx")
     page = excel["Sheet"]
     authors = {author.value for author in page["B"][1:]}
-    return render_template("authors.html", authors=authors)
+    return render_template("authors.html", authors=list(authors))
    
+
+@app.route("/add/", methods=["POST"])
+def add():
+    f = request.form
+    # print(f["author"], f["book"])
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sheet"]
+    last = len(page["A"]) + 1
+    page[f"A{last}"] = f["author"]
+    page[f"B{last}"] = f["book"]
+    excel.save("tales.xlsx")
+    return "форма получена"
