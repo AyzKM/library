f6fa64f4489a0441e8876556ca75266e1ff5a8f3
Ayzada Karataeva <ayzadakarataeva@Ayzadas-MBP.netis>
2021-02-04 18:45:41 +0100

    added books.html

--- app.py
+++ app.py
@@ -12,20 +12,26 @@ def books():
     excel = load_workbook("tales.xlsx")
     page = excel["Sheet"]
 
-    tales = [tale.value for tale in page["A"]][1:]
+    object_list = [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
+    print(object_list)
+    return render_template(
+        "books.html", object_list=object_list
+        )
 
-    authors = [author.value for author in page["B"]][1:]
+#     tales = [tale.value for tale in page["A"]][1:]
 
-    html = """
-        <a href="/authors">Авторы</a>
-        <a href="/books">Книги</a>
-        <h1>Тут будет список книг</h1>
-    """
+#     authors = [author.value for author in page["B"]][1:]
 
-    for i in range(len(tales)):
-        html += f"<h2>{tales[i]} - {authors[i]}</h2>"
+#     html = """
+#         <a href="/authors">Авторы</a>
+#         <a href="/books">Книги</a>
+#         <h1>Тут будет список книг</h1>
+#     """
 
-    return html 
+#     for i in range(len(tales)):
+#         html += f"<h2>{tales[i]} - {authors[i]}</h2>"
+
+#     return html 
 
 @app.route("/authors/")
 def authors():
