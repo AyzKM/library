21eb804e342e6f9ad45c7a4bd921360f3ec8fc00
Ayzada Karataeva <ayzadakarataeva@Ayzadas-MBP.netis>
2021-02-14 22:23:07 +0100

    1.added bootstrap to all html files, 2.added new font from fontgoogle, 3.created several new html files: homepage, book_edit, saved. 4.deleted index.html

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sheet"]
+    object_list = [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
+    return render_template("homepage.html", object_list=object_list)
+
 
 @app.route("/books/")
 def books():
@@ -39,19 +43,6 @@ def authors():
     page = excel["Sheet"]
     authors = {author.value for author in page["B"][1:]}
     return render_template("authors.html", authors=list(authors))
-   
-
-@app.route("/add/", methods=["POST"])
-def add():
-    f = request.form
-    # print(f["author"], f["book"])
-    excel = load_workbook("tales.xlsx")
-    page = excel["Sheet"]
-    last = len(page["A"]) + 1
-    page[f"A{last}"] = f["author"]
-    page[f"B{last}"] = f["book"]
-    excel.save("tales.xlsx")
-    return "форма получена"
 
 
 @app.route("/book/<num>/")
@@ -60,6 +51,54 @@ def book(num):
     page = excel["Sheet"]
     object_list = [[tale.value, tale.offset(column=1).value, tale.offset(column=2).value] for tale in page["A"][1:]]
     obj = object_list[int(num)]
+    obj.append(num)
     return render_template(
         "book.html", obj=obj
         )
+
+
+@app.route("/book/<num>/edit/")
+def book_edit(num):
+    num = int(num) + 2
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sheet"]
+    tale = page[f"A{num}"]
+    author = page[f"B{num}"]
+    image = page[f"C{num}"]
+    obj = [tale.value, author.value, image.value, num]
+    return render_template("book_edit.html", obj=obj)
+
+
+@app.route("/book/<num>/save/", methods=["POST", "GET"])
+def book_save(num):
+    if request.method == "POST":
+        num = int(num)
+        excel = load_workbook("tales.xlsx")
+        page = excel["Sheet"]
+        form = request.form
+        page[f"A{num}"] = form["tale"]
+        page[f"B{num}"] = form["author"]
+        page[f"C{num}"] = form["image"]
+        excel.save("tales.xlsx")
+        return render_template("saved.html")
+    else:
+        print("this method is get")
+
+@app.route("/add/")
+def book_add():
+    return render_template("book_add.html")
+
+@app.route("/save", methods=["POST", "GET"])
+def book_new_save():
+    if request.method == "POST":
+        f = request.form
+        print(f["author"], f["book"])
+        excel = load_workbook("tales.xlsx")
+        page = excel["Sheet"]
+        last = len(page["A"]) + 1
+        page[f"A{last}"] = f["book"]
+        page[f"B{last}"] = f["author"]
+        excel.save("tales.xlsx")
+        return "saved successfully"
+    else:
+        print("error")
